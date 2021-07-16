d048ae0541c5aea87265505342e04bbc17ced6c0
antim167 <singhantim505@gmail.com>
2021-07-15 22:01:59 -0700

    16jul

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import Layout from '../Layout'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faList, faTh } from '@fortawesome/free-solid-svg-icons'
 
 /**
 * @author
@@ -8,7 +10,35 @@ import Layout from '../Layout'
 **/
 
 class Search extends Component {
-  state = {}
+  //1.property
+  state = {
+    view: {
+      r: 100,
+      col1: 3,
+      col2: 9
+    }
+  }
+  //2 connstructor
+
+  //3 method
+  showGridView = () => {
+    this.setState({
+      view: {
+        r: '25 float-start',
+        col1: 12,
+        col2: 12
+      }
+    })
+  }
+  showListView = () => {
+    this.setState({
+      view: {
+        r: 100,
+        col1: 3,
+        col2: 9
+      }
+    })
+  }
   render() {
     return (
       <Layout>
@@ -68,7 +98,7 @@ class Search extends Component {
                     </a>
                   </li>
 
-                  <div id="demo" class="collapse">
+                  <div id="demo" className="collapse">
                     <li className="  list-group-item border-0 p-0 ">
                       <a href="#" className="text-dark ">
                         <input className="form-check-input me-1" type="checkbox" value="" />
@@ -138,23 +168,153 @@ class Search extends Component {
               </div>
             </div>
             <div className="col-9 tbdr ">
-              <div className="row m-0 mt-5 border-bottom">
-                <div className="col-3">
+            <div className="">
+                <ul className="nav float-end ">
+                  <li className="nav-item">
+                    <a className="nav-link text-dark  " href="#" onClick={this.showGridView}>
+                      <FontAwesomeIcon icon={faTh} />
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link text-dark " href="#" onClick={this.showListView}>
+                      <FontAwesomeIcon icon={faList} />
+                    </a>
+                  </li>
+                </ul>
+              </div>
+              <div className="a_search_filtertop border-bottom">
+                <h2>filter by price</h2>
+                <ul className="nav float start">
+                  <li className="nav-item">
+                    <a className="nav-link btn btn-light border text-dark " href="#">under $ 20,000</a>
+                  </li>
+                 
+                </ul>
+                </div>
+              
+
+              <div className={'row m-0 mt-5 border-bottom w-' + this.state.view.r}>
+                <div className={'col-' + this.state.view.col1}>
                   <a href="#"><img className=" img-fluid" src="./image/mainimagae/srchlap.jpg" /></a>
                 </div>
-                <div className="col-9   a_anch_dec">
+                <div className={'a_anch_dec col-' + this.state.view.col2}>
                   <h5><a href="#" className="a_srch_main_1_anch text-dark">Lenovo IdeaPad Slim 5 11th Gen Intel Core i5 15.6" FHD IPS Thin &amp; Light Laptop (8GB/256GB SSD + 1TB HDD/Win</a></h5>
-                  <p className="position-relative">
-                    <a href="#" className=" d-block  position-relative a_spritsheet_con a_srch_sprsht a_4star">
-                      <a href="#" className="d-block position-absolute arrow_con spritesheet a_dwn_arrow"></a>
+                  <p className="m-0">
+                    <a href="#"  ><span className="d-block float-start  a_spritsheet_con a_srch_sprsht a_4star"></span>
+                      <a href="#"> <span className="d-block  arrow_con spritesheet a_dwn_arrow float-start"></span></a>
+                      <a href="#" className=" text-dark">483</a>
                     </a>
-                    <a href="#" className="position-absolute glob_rating text-dark">483</a>
                   </p>
                   <span> <span className="fs-4 text-danger">₹37,990</span> <span className="text-decoration-line-through">₹39,990</span><span className="fw-bold" > Save ₹1,798 (5%)</span></span>
                   <span className="d-block">Save extra with No Cost EMI</span>
-                  <p className="position-relative">
-                    <a href="#" className=" d-block srch_prime_box prime_pos spritesheet"></a>
-                    <span className="position-absolute prm_text"><span> Get it by</span><span className="fw-bold"> Friday, July 16</span></span>
+                  <p className="m-0">
+                    <a href="#" className=""><span className=" d-block float-start srch_prime_box prime_pos spritesheet"></span></a>
+                    <span><span> Get it by</span><span className="fw-bold"> Friday, July 16</span></span>
+                  </p>
+                  <span>FREE Delivery by Amazon</span>
+                </div>
+              </div>
+              <div className={'row m-0 mt-5 border-bottom w-' + this.state.view.r}>
+                <div className={'col-' + this.state.view.col1}>
+                  <a href="#"><img className=" img-fluid" src="./image/mainimagae/srchlap.jpg" /></a>
+                </div>
+                <div className={'a_anch_dec col-' + this.state.view.col2}>
+                  <h5><a href="#" className="a_srch_main_1_anch text-dark">Lenovo IdeaPad Slim 5 11th Gen Intel Core i5 15.6" FHD IPS Thin &amp; Light Laptop (8GB/256GB SSD + 1TB HDD/Win</a></h5>
+                  <p className="m-0">
+                    <a href="#"  ><span className="d-block float-start  a_spritsheet_con a_srch_sprsht a_4star"></span>
+                      <a href="#"> <span className="d-block  arrow_con spritesheet a_dwn_arrow float-start"></span></a>
+                      <a href="#" className=" text-dark">483</a>
+                    </a>
+                  </p>
+                  <span> <span className="fs-4 text-danger">₹37,990</span> <span className="text-decoration-line-through">₹39,990</span><span className="fw-bold" > Save ₹1,798 (5%)</span></span>
+                  <span className="d-block">Save extra with No Cost EMI</span>
+                  <p className="m-0">
+                    <a href="#" className=""><span className=" d-block float-start srch_prime_box prime_pos spritesheet"></span></a>
+                    <span><span> Get it by</span><span className="fw-bold"> Friday, July 16</span></span>
+                  </p>
+                  <span>FREE Delivery by Amazon</span>
+                </div>
+              </div>
+              <div className={'row m-0 mt-5 border-bottom w-' + this.state.view.r}>
+                <div className={'col-' + this.state.view.col1}>
+                  <a href="#"><img className=" img-fluid" src="./image/mainimagae/srchlap.jpg" /></a>
+                </div>
+                <div className={'a_anch_dec col-' + this.state.view.col2}>
+                  <h5><a href="#" className="a_srch_main_1_anch text-dark">Lenovo IdeaPad Slim 5 11th Gen Intel Core i5 15.6" FHD IPS Thin &amp; Light Laptop (8GB/256GB SSD + 1TB HDD/Win</a></h5>
+                  <p className="m-0">
+                    <a href="#"  ><span className="d-block float-start  a_spritsheet_con a_srch_sprsht a_4star"></span>
+                      <a href="#"> <span className="d-block  arrow_con spritesheet a_dwn_arrow float-start"></span></a>
+                      <a href="#" className=" text-dark">483</a>
+                    </a>
+                  </p>
+                  <span> <span className="fs-4 text-danger">₹37,990</span> <span className="text-decoration-line-through">₹39,990</span><span className="fw-bold" > Save ₹1,798 (5%)</span></span>
+                  <span className="d-block">Save extra with No Cost EMI</span>
+                  <p className="m-0">
+                    <a href="#" className=""><span className=" d-block float-start srch_prime_box prime_pos spritesheet"></span></a>
+                    <span><span> Get it by</span><span className="fw-bold"> Friday, July 16</span></span>
+                  </p>
+                  <span>FREE Delivery by Amazon</span>
+                </div>
+              </div>
+              <div className={'row m-0 mt-5 border-bottom w-' + this.state.view.r}>
+                <div className={'col-' + this.state.view.col1}>
+                  <a href="#"><img className=" img-fluid" src="./image/mainimagae/srchlap.jpg" /></a>
+                </div>
+                <div className={'a_anch_dec col-' + this.state.view.col2}>
+                  <h5><a href="#" className="a_srch_main_1_anch text-dark">Lenovo IdeaPad Slim 5 11th Gen Intel Core i5 15.6" FHD IPS Thin &amp; Light Laptop (8GB/256GB SSD + 1TB HDD/Win</a></h5>
+                  <p className="m-0">
+                    <a href="#"  ><span className="d-block float-start  a_spritsheet_con a_srch_sprsht a_4star"></span>
+                      <a href="#"> <span className="d-block  arrow_con spritesheet a_dwn_arrow float-start"></span></a>
+                      <a href="#" className=" text-dark">483</a>
+                    </a>
+                  </p>
+                  <span> <span className="fs-4 text-danger">₹37,990</span> <span className="text-decoration-line-through">₹39,990</span><span className="fw-bold" > Save ₹1,798 (5%)</span></span>
+                  <span className="d-block">Save extra with No Cost EMI</span>
+                  <p className="m-0">
+                    <a href="#" className=""><span className=" d-block float-start srch_prime_box prime_pos spritesheet"></span></a>
+                    <span><span> Get it by</span><span className="fw-bold"> Friday, July 16</span></span>
+                  </p>
+                  <span>FREE Delivery by Amazon</span>
+                </div>
+              </div>
+              <div className={'row m-0 mt-5 border-bottom w-' + this.state.view.r}>
+                <div className={'col-' + this.state.view.col1}>
+                  <a href="#"><img className=" img-fluid" src="./image/mainimagae/srchlap.jpg" /></a>
+                </div>
+                <div className={'a_anch_dec col-' + this.state.view.col2}>
+                  <h5><a href="#" className="a_srch_main_1_anch text-dark">Lenovo IdeaPad Slim 5 11th Gen Intel Core i5 15.6" FHD IPS Thin &amp; Light Laptop (8GB/256GB SSD + 1TB HDD/Win</a></h5>
+                  <p className="m-0">
+                    <a href="#"  ><span className="d-block float-start  a_spritsheet_con a_srch_sprsht a_4star"></span>
+                      <a href="#"> <span className="d-block  arrow_con spritesheet a_dwn_arrow float-start"></span></a>
+                      <a href="#" className=" text-dark">483</a>
+                    </a>
+                  </p>
+                  <span> <span className="fs-4 text-danger">₹37,990</span> <span className="text-decoration-line-through">₹39,990</span><span className="fw-bold" > Save ₹1,798 (5%)</span></span>
+                  <span className="d-block">Save extra with No Cost EMI</span>
+                  <p className="m-0">
+                    <a href="#" className=""><span className=" d-block float-start srch_prime_box prime_pos spritesheet"></span></a>
+                    <span><span> Get it by</span><span className="fw-bold"> Friday, July 16</span></span>
+                  </p>
+                  <span>FREE Delivery by Amazon</span>
+                </div>
+              </div>
+              <div className={'row m-0 mt-5 border-bottom w-' + this.state.view.r}>
+                <div className={'col-' + this.state.view.col1}>
+                  <a href="#"><img className=" img-fluid" src="./image/mainimagae/srchlap.jpg" /></a>
+                </div>
+                <div className={'a_anch_dec col-' + this.state.view.col2}>
+                  <h5><a href="#" className="a_srch_main_1_anch text-dark">Lenovo IdeaPad Slim 5 11th Gen Intel Core i5 15.6" FHD IPS Thin &amp; Light Laptop (8GB/256GB SSD + 1TB HDD/Win</a></h5>
+                  <p className="m-0">
+                    <a href="#"  ><span className="d-block float-start  a_spritsheet_con a_srch_sprsht a_4star"></span>
+                      <a href="#"> <span className="d-block  arrow_con spritesheet a_dwn_arrow float-start"></span></a>
+                      <a href="#" className=" text-dark">483</a>
+                    </a>
+                  </p>
+                  <span> <span className="fs-4 text-danger">₹37,990</span> <span className="text-decoration-line-through">₹39,990</span><span className="fw-bold" > Save ₹1,798 (5%)</span></span>
+                  <span className="d-block">Save extra with No Cost EMI</span>
+                  <p className="m-0">
+                    <a href="#" className=""><span className=" d-block float-start srch_prime_box prime_pos spritesheet"></span></a>
+                    <span><span> Get it by</span><span className="fw-bold"> Friday, July 16</span></span>
                   </p>
                   <span>FREE Delivery by Amazon</span>
                 </div>
